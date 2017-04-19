8f89130daf0a8f4a8af284ad74ce434daf413f18
OriolGarcia <130761.clot@fje.edu>
2017-04-19 19:37:03 +0200

    commit

--- models.py
+++ models.py
@@ -2,7 +2,14 @@
 
 from openerp import models, fields, api
 
-# class loan(models.Model):
-#     _name = 'loan.loan'
-
-#     name = fields.Char()
+ class loan(models.Model):
+     _name = 'loan.loan'
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+    capital = fields.float(digits=(6, 2), help="Capital de prestec")
+    interesos= fields.float(digits=(6, 2), help="Interessos")
+    numquotes= fields.Integer(string="Numero de quotes")
+    
+    @api.one
+	def calcula(self):
+		#Codi per fer càlculs
