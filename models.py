76a0c6f97b8b1e206cdcd32995608116d1e48808
OriolGarcia <130761.clot@fje.edu>
2017-04-21 11:53:18 +0200

    commit

--- models.py
+++ models.py
@@ -8,8 +8,13 @@ from openerp import models, fields, api
     description = fields.Text()
     capital = fields.float(digits=(6, 2), help="Capital de prestec")
     interesos= fields.float(digits=(6, 2), help="Interessos")
-    numquotes= fields.Integer(string="Numero de quotes")
-    
+    numquotes= fields.Integer(string="Numero de quotes",help="numero de quoetes")
+    quota= fields.float('Preu quota', {'readonly': True})
     @api.one
 	def calcula(self):
-		#Codi per fer càlculs
+		self.ensure_one()
+	#Generates a random password between 12 and 15 characters long and writes it to the record.
+	self.write({
+	    'quota': capital/((1-(1+interessos)**numquotes)/interessos)
+	    
+	})
